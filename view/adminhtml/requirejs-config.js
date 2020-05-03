370f3a3f28f99174e4d5dd24d20b0113018d2c7a
Ismael Vacco <isma.vacco@gmail.com>
2020-05-03 17:53:40 -0300

    module magento 2 gocache

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,14 @@
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+
+var config = {
+    map: {
+        '*': {
+            testToken: 'GoCache_CDN/js/testtoken'
+        }
+    }
+};
